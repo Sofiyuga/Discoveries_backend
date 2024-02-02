6faa466cadfeb5e251211663af7109e6c4aca6c8
Sofiyuga <114177738+Sofiyuga@users.noreply.github.com>
2024-02-03 02:31:13 +0300

    Delete discoveries directory

--- discoveries/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Pioneer)
-admin.site.register(Discovery)
-admin.site.register(CustomUser)
-
--- discoveries/app/management/commands/clean_db.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from ...models import *
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        Pioneer.objects.all().delete()
-        Discovery.objects.all().delete()
-        CustomUser.objects.all().delete()
